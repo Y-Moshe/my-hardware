cb839c3f2f587f8f8e26cbde8a8ceb39245b364f
Y-Moshe <moshe.nehemiah@gmail.com>
2023-07-08 22:39:44 +0300

    Define main application layouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
-import { Component, computed, effect } from '@angular/core'
+import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
-import { Systeminformation as SI } from 'systeminformation'
+import { RouterOutlet } from '@angular/router'
 
 import { AppFooterComponent, AppHeaderComponent } from '@/components'
-import { CpuService } from '@/services/cpu.service'
-import { CpuStatus } from '@/types'
-import { RouterModule, RouterOutlet } from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -13,12 +10,4 @@ import { RouterModule, RouterOutlet } from '@angular/router'
   templateUrl: './app.component.html',
   imports: [CommonModule, AppHeaderComponent, AppFooterComponent, RouterOutlet],
 })
-export class AppComponent {
-  cpuData = computed<SI.CpuData | null>(() => this._cpuService.cpuData())
-  cpuStatus = computed<CpuStatus | null>(() => this._cpuService.cpuStatus())
-
-  constructor(private readonly _cpuService: CpuService) {
-    this._cpuService.startService(1000)
-    effect(() => console.log(this.cpuStatus()))
-  }
-}
+export class AppComponent {}
--- src/app/components/app-header/app-header.component.ts
+++ src/app/components/app-header/app-header.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterLink } from '@angular/router'
+import { NgIconComponent, provideIcons } from '@ng-icons/core'
+import { bootstrapCpu, bootstrapThreeDotsVertical } from '@ng-icons/bootstrap-icons'
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgIconComponent],
   templateUrl: './app-header.component.html',
+  viewProviders: [provideIcons({ bootstrapCpu, bootstrapThreeDotsVertical })],
 })
 export class AppHeaderComponent {}
