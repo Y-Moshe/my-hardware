67d11536caac5180ae0e2ffa90cb54ce32b48c56
Y-Moshe <moshe.nehemiah@gmail.com>
2023-07-08 22:39:56 +0300

    Code improve

--- src/app/services/cpu.service.ts
+++ src/app/services/cpu.service.ts
@@ -1,45 +1,48 @@
-import { Injectable, computed, signal } from '@angular/core';
-import { ElectronService } from 'ngx-electron';
+import { Injectable, signal } from '@angular/core'
+import { ElectronService } from 'ngx-electron'
 
-import { Systeminformation as SI } from 'systeminformation';
-import { CpuStatus } from '@/types';
+import { Systeminformation as SI } from 'systeminformation'
+import { CpuStatus } from '@/types'
 
 @Injectable({
   providedIn: 'root',
 })
 export class CpuService {
-  cpuData = signal<SI.CpuData | null>(null);
-  cpuStatus = signal<CpuStatus | null>(null);
+  cpuData = signal<SI.CpuData | null>(null)
+  cpuStatus = signal<CpuStatus | null>(null)
 
-  intervalId?: NodeJS.Timer;
+  intervalId: NodeJS.Timer | null = null
 
   constructor(private readonly _electronService: ElectronService) {
-    this._getCpuData().then((data) => this.cpuData.set(data));
+    this._getCpuData().then((data) => this.cpuData.set(data))
   }
 
   public startService(refreshRate: number): void {
     this.intervalId = setInterval(async () => {
-      const status = await this._getCpuStatus();
-      this.cpuStatus.set(status);
-    }, refreshRate);
+      const status = await this._getCpuStatus()
+      this.cpuStatus.set(status)
+    }, refreshRate)
   }
 
   public stopService(): void {
-    if (this.intervalId) clearInterval(this.intervalId);
+    if (this.intervalId) {
+      clearInterval(this.intervalId)
+      this.intervalId = null
+    }
   }
 
   public restartService(refreshRate: number): void {
-    if (this.intervalId) clearInterval(this.intervalId);
-    this.startService(refreshRate);
+    this.stopService()
+    this.startService(refreshRate)
   }
 
   // Private methods
 
   private _getCpuData(): Promise<SI.CpuData> {
-    return this._electronService.ipcRenderer.invoke('getCpuData');
+    return this._electronService.ipcRenderer.invoke('getCpuData')
   }
 
   private _getCpuStatus(): Promise<CpuStatus> {
-    return this._electronService.ipcRenderer.invoke('getCpuStatus');
+    return this._electronService.ipcRenderer.invoke('getCpuStatus')
   }
 }
