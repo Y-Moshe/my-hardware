23247079d4902d0ee723d6bfe6e93204b8671b90
Y-Moshe <moshe.nehemiah@gmail.com>
2023-07-26 15:31:44 +0300

    Improving info-table component with headers option

--- src/app/components/info-table/info-table.component.ts
+++ src/app/components/info-table/info-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, OnInit } from '@angular/core'
 import { CommonModule, KeyValue } from '@angular/common'
 
 @Component({
@@ -7,10 +7,20 @@ import { CommonModule, KeyValue } from '@angular/common'
   imports: [CommonModule],
   templateUrl: './info-table.component.html',
 })
-export class InfoTableComponent {
+export class InfoTableComponent implements OnInit {
+  @Input() headers: string[] | null = null
+  /** Required if headers passed to the component! */
+  @Input() rows: (string | number)[][] | null = null
   @Input() informations: Record<string, string | number> = {}
 
-  trackByIndex(index: number, item: KeyValue<string, string | number>) {
+  ngOnInit(): void {
+    this.headers && this.headers.unshift('')
+  }
+
+  trackByIndex(
+    index: number,
+    item: KeyValue<string, string | number> | (string | number)[]
+  ) {
     return index
   }
 
--- src/app/pages/disk-page/disk-page.component.ts
+++ src/app/pages/disk-page/disk-page.component.ts
@@ -32,29 +32,35 @@ export class DiskPageComponent implements OnInit {
 
   disksInfoTable = computed(() => {
     const fsSize = this.disksStatus()?.fsSize
-    if (!fsSize) return {} as Record<string, string | number>
+    if (!fsSize) return []
 
-    const info = fsSize.reduce<Record<string, string | number>>(
+    const totals = fsSize.reduce(
       (prev, curr) => {
-        prev['Total Size'] = (prev['Total Size'] as number) + curr.size
-        prev['Total Used'] = (prev['Total Used'] as number) + curr.used
-        prev['Total Free'] = (prev['Total Free'] as number) + curr.available
-        prev[`${curr.mount} Size`] = curr.size
-        prev[`${curr.mount} Used`] = curr.used
-        prev[`${curr.mount} Free`] = curr.available
+        prev.size += curr.size
+        prev.used += curr.used
+        prev.free += curr.available
         return prev
       },
       {
-        'Total Size': 0,
-        'Total Used': 0,
-        'Total Free': 0,
+        size: 0,
+        used: 0,
+        free: 0,
       }
     )
 
-    Object.keys(info).forEach(
-      (key) => (info[key] = this._bytesTo.transform(info[key] as number))
-    )
+    const info = fsSize.map((drive) => [
+      drive.mount,
+      this._bytesTo.transform(drive.size),
+      this._bytesTo.transform(drive.used),
+      this._bytesTo.transform(drive.available),
+    ])
 
+    info.unshift([
+      'Total Usage',
+      this._bytesTo.transform(totals.size),
+      this._bytesTo.transform(totals.used),
+      this._bytesTo.transform(totals.free),
+    ])
     return info
   })
 
