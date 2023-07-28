2b4508750e7c4c2b13c2c0e5721a3096eb328a29
Y-Moshe <moshe.nehemiah@gmail.com>
2023-07-28 18:41:35 +0300

    Simple Home page

--- src/app/pages/home/home-page.component.ts
+++ src/app/pages/home/home-page.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core'
 import { CommonModule } from '@angular/common'
+import { CpuPageComponent } from '../cpu-page/cpu-page.component'
+import { MemoryPageComponent } from '../memory-page/memory-page.component'
+import { DiskPageComponent } from '../disk-page/disk-page.component'
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    CpuPageComponent,
+    MemoryPageComponent,
+    DiskPageComponent,
+  ],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {}
